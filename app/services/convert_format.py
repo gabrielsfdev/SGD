7115775ba69c5c2f8d5bd1db2576bebf8851e23f
gabrielsfdev <ferreirawdev@gmail.com>
2024-04-17 20:34:29 -0300

    Ajustado função que converte HEIC em BRG

--- app/services/convert_format.py
+++ app/services/convert_format.py
@@ -2,6 +2,9 @@
 from PIL import Image
 import numpy as np
 import cv2
+from pillow_heif import register_heif_opener
+
+register_heif_opener()
 
 def format_identificator(img_path):
     # img = Image.fromarray(img)
