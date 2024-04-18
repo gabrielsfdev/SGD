bb8979ef4064dbe81bfe150f3963cbb1e9d62d8a
gabrielsfdev <ferreirawdev@gmail.com>
2024-04-17 23:53:37 -0300

    Adicionando onde ficará os campos extraídos

--- app/services/OCR_service.py
+++ app/services/OCR_service.py
@@ -8,16 +8,25 @@ class OCR_DOCS:
     def __init__(self, img_path) -> None:
         self.img = format_identificator(img_path)
         self.extracted = ""
+        self.name = ""
+        self.Rg_id = ""
+        self.born_date = ""
+        self.mother_name = ""
+        self.father_name = ""
+        self.place_of_birth = ""
+        self.date_of_issue = ""
+        
 
     def new_rg(self):
-        imagem_cinza = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        rgb = cv2.cvtColor(self.img, cv2.COLOR_RGB2BGR)
+        imagem_cinza = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
         bilateral = cv2.bilateralFilter(imagem_cinza, 20, 75, 75)
         cv2.imshow("bilateral", bilateral)
         margin = cv2.Canny(bilateral, 50, 100, apertureSize=3) # Apenas em desenvolvimento (não é necessário para o OCR)
         cv2.imshow("img", margin) # Apenas em desenvolvimento
         cv2.waitKey(0)
 
-        self.extracted = pytesseract.image_to_string(bilateral, lang='por', config='--psm 6')
+        self.extracted = pytesseract.image_to_string(imagem_cinza, lang='por', config='--psm 6')
     
     def find_info(self, info):
         regex = rf"{info} .+\n(.+)\b" # Exemplo de regex para buscar informação na linha de baixo
@@ -26,13 +35,12 @@ class OCR_DOCS:
             return info_catched[0]
         else:
             return "Não encontrado"
-
-
 if __name__ == "__main__":
     # Colocar Testagem em outro lugar em próxima sprint
-    teste = OCR_DOCS("novoRg.png")
+    teste = OCR_DOCS("ambiente_virtual/rg_verso.jpg")
     teste.new_rg()
 
     print(teste.extracted)
     print('------------------------------------')
-    print(teste.find_info("Nome"))
+    # teste.extract_info()
+    print(teste.mother_name)
